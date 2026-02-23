480a6dfb792e3c97c6420bde18301ff3754515c5
Theekshana Jayalath <it23841918@my.sliit.lk>
2026-02-23 22:21:35 +0530

    added example demonstrating JS promises

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const https = require('https');
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-let data = '';
-resp.on('data', (chunk) => {
-data += chunk;
+const myPromise = new Promise((resolve, reject) => {
+if (condition) {
+resolve('Success!');
+} else {
+reject('Failure!');
+}
 });
-resp.on('end', () => {
-console.log(JSON.parse(data));
-});
-}).on('error', (err) => {
-console.log("Error: " + err.message);
+myPromise.then((result) => {
+console.log(result);
+}).catch((error) => {
+console.log(error);
 });
